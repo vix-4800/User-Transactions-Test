04d61d97516b6a38d0972cea5f69cb730af9dc61
Anton Vix <Gordan.ei@protonmail.ch>
2025-02-21 16:27:37 +0400

    Transaction model

---
+++ Models/Transaction.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+class Transaction
+{
+	public function __construct(
+		private int $id,
+		private int $accountFrom,
+		private int $accountTo,
+		private int|float $amount,
+		private DateTime $date,
+	) {
+		// 
+	}
+
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+	public function getAccountFrom(): int
+	{
+		return $this->accountFrom;
+	}
+
+	public function getAccountTo(): int
+	{
+		return $this->accountTo;
+	}
+
+	public function getAmount(): int
+	{
+		return $this->amount;
+	}
+
+	public function getDate(): DateTime
+	{
+		return $this->date;
+	}
+}
